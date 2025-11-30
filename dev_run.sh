40509a795be16b6323bc245d78f0d29e56ea3adc
UwU <uwu.fionia@gmail.com>
2025-11-30 20:45:03 +0800

    latest

--- dev_run.sh
+++ dev_run.sh
@@ -6,14 +6,17 @@ echo "------------------------------------------------"
 echo "--- Starting Update Auto-Build Process ---"
 echo "------------------------------------------------"
 
-echo ">> [STEP 1/3] Installing Vue Dependencies..."
+echo ">> [STEP 1/4] Installing Vue Dependencies..."
 npm --prefix ./vite install
 
-echo ">> [STEP 2/3] Building HTML,CSS,JS Assets..."
+echo ">> [STEP 2/4] Building HTML,CSS,JS Assets..."
 npm --prefix ./vite run build
 
-echo ">> [STEP 3/3] Cleaning and Packaging Java..."
+echo ">> [STEP 3/4] Cleaning and Packaging Java..."
 ./mvnw clean package
+
+echo ">> [STEP 4/4] Running up Java..."
+java -jar /target/base-0.0.1-SNAPSHOT.jar
 
 echo "------------------------------------------------"
 echo "---Finished Update Auto-Build Process ---"
