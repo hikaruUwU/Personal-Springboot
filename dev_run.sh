f1a1a4ad955a8ec2f43627617f0f4ed75004a0bf
UwU <uwu.fionia@gmail.com>
2025-12-02 15:42:46 +0800

    test-5

--- dev_run.sh
+++ dev_run.sh
@@ -13,7 +13,7 @@ echo ">> [STEP 2/4] Building HTML,CSS,JS Assets..."
 npm --prefix ./vite run build
 
 echo ">> [STEP 3/4] Cleaning and Packaging Java..."
-./mvnw clean package
+./mvnw clean package -DskipTests
 
 echo ">> [STEP 4/4] Running up Java..."
 java -jar ./target/base-0.0.1-SNAPSHOT.jar
