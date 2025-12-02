e98dea74393c4ccc2c395480e1a7a6c78c51f238
UwU <uwu.fionia@gmail.com>
2025-12-02 15:44:08 +0800

    test-6

--- src/main/java/com/demo/base/config/SQLLog4j2Config.java
+++ src/main/java/com/demo/base/config/SQLLog4j2Config.java
@@ -5,23 +5,22 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @Log4j2
 @Configuration
 public class SQLLog4j2Config {
-    private final DataSource dataSource;
-
-    public SQLLog4j2Config(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+//    private final DataSource dataSource;
+//
+//    public SQLLog4j2Config(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @PostConstruct
     public void init() throws SQLException {
         AuditManager.setAuditEnable(true);
 
-        dataSource.getConnection().close();
+        //dataSource.getConnection().close();
 
         AuditManager.setMessageCollector(auditMessage -> log.info("SQL PLAN >>> {}", auditMessage));
     }
