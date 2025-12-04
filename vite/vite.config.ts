ac1ba7080da58ace913c6b549366f5e970809c78
UwU <uwu.fionia@gmail.com>
2025-12-04 15:20:09 +0800

    feat:with basic login component demo

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     },
     build:{
         emptyOutDir: true,
-        //outDir: '../src/main/resources/static'  # using this for springboot
-        outDir: 'dist'
+        outDir: '../src/main/resources/static'
+        //outDir: 'dist'
     }
 })
